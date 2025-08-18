4f258a2058ccb1479412c1910a2f4cd9950e37b2
XingRay <leixing1012@gmail.com>
2025-08-19 00:39:31 +0800

    1 优化cmake脚本

--- cmake/import/import_vklite.cmake
+++ cmake/import/import_vklite.cmake
@@ -1,5 +1,6 @@
 # vklite
 # https://github.com/XingRay/vklite
+# set(vklite_install_dir "D:/code/git/github/vklite/build/dist/vklite" CACHE STRING "vklite install dir")
 function(import_vklite vklite_install_dir)
     message("import_vklite: vklite_install_dir: ${vklite_install_dir}")
     set(vklite_DIR ${vklite_install_dir}/lib/cmake)
